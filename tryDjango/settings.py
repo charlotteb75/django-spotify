491d9f9ebc57586975141792fbc6bb4bf8d7ba93
charlotteb75 <charlotte_brama@yahoo.fr>
2021-03-05 18:48:58 +0100

    add heroku host

--- tryDjango/settings.py
+++ tryDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(^fa((@lqzg*1mf#41^2^so8x291)(2v2e1v8!6l=*f=+l6#i*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'spotify-metadata-django.herokuapp.com']
 
 
 # Application definition
